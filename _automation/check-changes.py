375a6d5d15e69a4b8d978d5845a14d9639d0164c
ljvdhooft <ljvdhooft@gmail.com>
2023-04-01 11:10:18 +0200

    testing

--- _automation/check-changes.py
+++ _automation/check-changes.py
@@ -50,6 +50,9 @@ for change in changes:
 
 print(result)
 
+with open('/diff.snap', "w") as jsonFile:
+    json.dump(result, jsonFile)
+
 out = merge(snap, result)
 
 # with open("global/snapshots/diff.snap", "w") as jsonFile:
@@ -61,6 +64,6 @@ subfolders = [f.path for f in os.scandir(path) if f.is_dir()]
 for subfolder in subfolders:
     print("File updated in " + subfolder)
     with open(subfolder + '/P.FOH 2023.snap', "w") as jsonFile:
-        json.dump(out, jsonFile, sort_keys=True, indent=4)
+        json.dump(out, jsonFile)
 
 # os.remove("global/snapshots/changes.snap")
